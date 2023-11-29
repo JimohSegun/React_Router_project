4b9262dec1409e417ce3df9c8f31cbcdc56f5766
Segun Thompson Jimoh <segunjimoh13@gmail.com>
2023-11-29 08:04:12 +0100

    Update api.js

--- api.js
+++ api.js
@@ -20,18 +20,6 @@ const firebaseConfig = {
   measurementId: "G-REYV6K3S1T"
 };
 
-
-
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyD_k3v3HK3tKEqhlqFHPkwogW7PqEqhGhk",
-//     authDomain: "vanlife-a1af5.firebaseapp.com",
-//     projectId: "vanlife-a1af5",
-//     storageBucket: "vanlife-a1af5.appspot.com",
-//     messagingSenderId: "803007000356",
-//     appId: "1:803007000356:web:446cd3a1ca406839258db1"
-// };
-
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
 
